ad3f61d59481b528700746a2daf993649f3fbe73
Nguyen Tan Tai <tainguyen.uit@gmail.com>
2020-08-12 21:44:18 +0700

    Updates

---
+++ precache-manifest.cb83ecb412a0608fe375b0bc28f84b15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179f347b2fd2a05cd7a4f1860cb671ad",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ad5aa45abc83b1e09081",
+    "url": "/my-app/static/css/main.4d4ad824.chunk.css"
+  },
+  {
+    "revision": "e891b4d8512239016a12",
+    "url": "/my-app/static/js/2.09785f65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.09785f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5aa45abc83b1e09081",
+    "url": "/my-app/static/js/main.8b450816.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
